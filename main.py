c39ea1c9c5ac45998a6a0e6385b05dbfbf2ffd16
SW <vicjames@naver.com>
2023-10-19 05:25:47 +0900

    version 1.0

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from CXR_LLAMA.CXR_LLAMA import CXR_LLAMA_Loader
 from PIL import Image
 if __name__ == '__main__':
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-    model_path = "INPUT_MODEL_WEIGHT_PATH!"
+    model_path = "INPUT_MODEL_WEIGHT_PATH"
     img = Image.open(os.path.join(os.path.dirname(__file__), "IMG", "img.jpg"))
     loader = CXR_LLAMA_Loader(model_path=model_path, temperature=0.4, top_p=0.8)
 
