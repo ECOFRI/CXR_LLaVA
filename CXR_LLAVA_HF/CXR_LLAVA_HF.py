3b6e9884d0976566c93d12afa019d4b6c2b13106
SW <vicjames@naver.com>
2024-01-17 21:23:51 +0900

    version 2.0 / hugging-face AutoModel implementation

--- CXR_LLAVA_HF/CXR_LLAVA_HF.py
+++ CXR_LLAVA_HF/CXR_LLAVA_HF.py
@@ -7,6 +7,7 @@ from functools import partial
 from transformers import TextIteratorStreamer
 from transformers import StoppingCriteria, GenerationConfig
 from threading import Thread
+from dataclasses import dataclass
 
 # Model Constants
 IGNORE_INDEX = -100
@@ -19,6 +20,11 @@ class AttrDict(dict):
     def __init__(self, *args, **kwargs):
         super(AttrDict, self).__init__(*args, **kwargs)
         self.__dict__ = self
+    def __getattr__(self, key):
+        if key in self:
+            return self[key]
+        raise AttributeError(f"'AttrDict' object has no attribute '{key}'")
+
 
 class CXRLLAVAConfig(PretrainedConfig):
     model_type = "CXR-LLAVA"
@@ -45,7 +51,6 @@ class CXRLLAVAModel(PreTrainedModel):
         self.tokenizer.cls_token = self.tokenizer.unk_token
         self.tokenizer.mask_token = self.tokenizer.unk_token
 
-        from open_clip.model import CLIPVisionCfg
         vision_cfg = CLIPVisionCfg(**config.clip_vision_cfg)
 
         self.generation_config =  GenerationConfig.from_pretrained(config._name_or_path)
@@ -669,3 +674,32 @@ class KeywordsStoppingCriteria(StoppingCriteria):
             if keyword in outputs:
                 return True
         return False
+@dataclass
+class CLIPVisionCfg:
+    layers: Union[Tuple[int, int, int, int], int] = 12
+    width: int = 768
+    head_width: int = 64
+    mlp_ratio: float = 4.0
+    patch_size: int = 16
+    image_size: Union[Tuple[int, int], int] = 224
+
+    ls_init_value: Optional[float] = None  # layer scale initial value
+    patch_dropout: float = 0.  # what fraction of patches to dropout during training (0 would mean disabled and no patches dropped) - 0.5 to 0.75 recommended in the paper for optimal results
+    attentional_pool: bool = False  # whether to use attentional pooler in the last embedding layer (overrides pool_type)
+    attn_pooler_queries: int = 256  # n_queries for attentional pooler
+    attn_pooler_heads: int = 8  # n heads for attentional_pooling
+    no_ln_pre: bool = False  # disable pre transformer LayerNorm
+    pos_embed_type: str = 'learnable'
+    final_ln_after_pool: bool = False  # apply final LayerNorm after pooling
+    pool_type: str = 'tok'
+    output_tokens: bool = False
+    act_kwargs: Optional[dict] = None
+    norm_kwargs: Optional[dict] = None
+
+    timm_model_name: Optional[str] = None  # a valid model name overrides layers, width, patch_size
+    timm_model_pretrained: bool = False  # use (imagenet) pretrained weights for named model
+    timm_pool: str = 'avg'  # feature pooling for timm model ('abs_attn', 'rot_attn', 'avg', '')
+    timm_proj: str = 'linear'  # linear projection for timm model output ('linear', 'mlp', '')
+    timm_proj_bias: bool = False  # enable bias final projection
+    timm_drop: float = 0.  # head dropout
+    timm_drop_path: Optional[float] = None  # backbone stochastic depth
